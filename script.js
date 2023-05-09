13b38eb0f153adbb2ecc356aa258bb5f1d0e7742
Eximo <eximo888@gmail.com>
2023-05-09 17:50:36 +0200

    style changes to css

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ form.addEventListener('submit', (event) => {
   updateViewWithArr(myLibrary);
   form.reset();
 });
+function bookFactory(title, author, pages, isRead) {}
 
 function Book(title, author, pages, isRead) {
   this.title = title;
@@ -35,11 +36,12 @@ function Book(title, author, pages, isRead) {
     this.isRead = !this.isRead;
     updateReadStatusInView(this.id);
   };
-  this.deleteSelf = function () {
-    deleteBookFromView(this.id);
-    delete myLibrary[myLibrary.map((x) => x.id).indexOf(this.id)];
-  };
 }
+
+Book.prototype.deleteSelf = function () {
+  deleteBookFromView(this.id);
+  delete myLibrary[myLibrary.map((x) => x.id).indexOf(this.id)];
+};
 
 function updateReadStatusInView(bookIndex) {
   const el = document.querySelector(`[data-index="${bookIndex}"]`);
@@ -77,12 +79,12 @@ function createCard(book) {
                 <div class="book-by">
                     <span class="book-author">${book.author}</span>
                 </div>
-            <div class="book-pages">${book.pages}</div>
+            <div class="book-pages">#${book.pages}</div>
     `;
   const btn = document.createElement('button');
   btn.className = 'read-btn';
   btn.innerHTML =
-    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>sync</title><path d="M12,18A6,6 0 0,1 6,12C6,11 6.25,10.03 6.7,9.2L5.24,7.74C4.46,8.97 4,10.43 4,12A8,8 0 0,0 12,20V23L16,19L12,15M12,4V1L8,5L12,9V6A6,6 0 0,1 18,12C18,13 17.75,13.97 17.3,14.8L18.76,16.26C19.54,15.03 20,13.57 20,12A8,8 0 0,0 12,4Z" /></svg>';
+    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Read/Unread</title><path d="M12,18A6,6 0 0,1 6,12C6,11 6.25,10.03 6.7,9.2L5.24,7.74C4.46,8.97 4,10.43 4,12A8,8 0 0,0 12,20V23L16,19L12,15M12,4V1L8,5L12,9V6A6,6 0 0,1 18,12C18,13 17.75,13.97 17.3,14.8L18.76,16.26C19.54,15.03 20,13.57 20,12A8,8 0 0,0 12,4Z" /></svg>';
   btn.addEventListener('click', () => {
     book.changeStatus();
   });
